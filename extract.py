69ccc0e394ae9520f193e12a8046d34314e38321
smigii <smigii@protonmail.com>
2021-11-21 22:34:58 -0600

    Extracting relatives seems to be working
    Further testing needed

--- extract.py
+++ extract.py
@@ -19,9 +19,10 @@ def extract_children_parents(data, df, key_id):
 	list_family = str(data)[4:-5].split('<br/>')
 
 	for person in list_family:
-		# ---------------------- Yikes -----------------------
+
 		sub_soup = BeautifulSoup(person.strip(), 'html.parser')
 
+		# Check if the person is in anchor tag (in imdb database)
 		if len(sub_soup.find_all("a")) == 0:
 			# Non-linked entries are stored [lastname, firstname]
 			person_name = person.split(',')
@@ -33,6 +34,48 @@ def extract_children_parents(data, df, key_id):
 		else:
 			imdb_id_person = sub_soup.find("a", href=True)["href"].replace("/name/", '')
 			df.loc[len(df)] = [key_id, imdb_id_person, sub_soup.text.strip()]
+
+
+def extract_relatives(data, df, key_id):
+	"""
+	:param data: <td> wrapped soup containing section data
+	:param df: DataFrame to update
+	:param key_id: Current imdb id being processed
+	"""
+
+	# [4:-5] removes the <td> and </td> tags
+	relatives = str(data)[4:-5].split('<br/>')
+
+	for relative in relatives:
+
+		sub_soup = BeautifulSoup(relative, 'html.parser')
+
+		# Check if the person is in anchor tag (in imdb database)
+		if len(sub_soup.find_all("a")) == 0:
+			# Non-linked entries are stored [lastname, firstname\n (relation)]
+			content = relative.strip().split('\n')
+
+			relation = ""
+			if len(content) > 1:
+				relation = content[1].strip().strip('()')
+
+			person_name = content[0].split(',')
+			person_name = [name.strip() for name in person_name]
+			person_name.reverse()
+			person_name = ' '.join(person_name)
+
+			df.loc[len(df)] = [key_id, "", person_name, relation]
+
+		else:
+			relation = ""
+			relation_split = relative.strip().split('</a>')
+			if len(relation_split) > 1:
+				relation = relation_split[1].strip().strip('()')
+
+			anchor = sub_soup.find("a", href=True)
+			imdb_id_person = anchor["href"].replace("/name/", '')
+
+			df.loc[len(df)] = [key_id, imdb_id_person, anchor.text.strip(), relation]
 
 
 # -----------------------------------------------------------------------------
@@ -47,7 +90,8 @@ df_parents = pd.DataFrame(columns=["imdb_id", "parent_id", "parent_name"])
 df_relatives = pd.DataFrame(columns=["imdb_id", "relative_id", "relative_name", "relation"])
 
 # Open and get html table data
-imdb_id = "nm0000226"
+# imdb_id = "nm0000226"  # Will Smith
+imdb_id = "nm0565250"  # Melissa McCarthy, famous relatives
 f = open("html/" + imdb_id)
 soup = BeautifulSoup(f.read(), 'lxml')
 
@@ -76,7 +120,7 @@ for row in rows:
 		extract_children_parents(columns[1], df_parents, imdb_id)
 
 	elif section == "Relatives":
-		print("Relatives!")
+		extract_relatives(columns[1], df_relatives, imdb_id)
 
 	else:
 		print("Oh fuck!")
